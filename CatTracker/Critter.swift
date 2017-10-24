997fa08362743758aa044011410ecb3259387782
hugolkw <33016540+hugolkw@users.noreply.github.com>
2017-10-24 11:42:05 +0800

    archiving set up of Critter
    done

--- CatTracker/Critter.swift
+++ CatTracker/Critter.swift
@@ -5,14 +5,26 @@
 //  Created by HUGOLEE on 23/10/2017.
 //  Copyright © 2017 edu.cuhk.cmsc5736.hugolkw. All rights reserved.
 //
-
+import os.log
 import UIKit
 
-class Critter {
+class Critter: NSObject, NSCoding  {
     //MARK: Properties
     var name: String
     var photo: UIImage?
     var details: String
+    //MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(
+        for: .documentDirectory, in: .userDomainMask ).first
+    static let ArchiveURL =
+        DocumentsDirectory?.appendingPathComponent("critters",
+            isDirectory: false)
+    //MARK: Types
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let details = "details"
+    }
     //MARK: Initialization
     init?(name: String, photo: UIImage?, details: String) {
         if name.isEmpty {
@@ -21,5 +33,26 @@ class Critter {
         self.name = name
         self.photo = photo
         self.details = details
+    }
+    
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey:PropertyKey.name)
+        aCoder.encode(photo, forKey:PropertyKey.photo)
+        aCoder.encode(details, forKey: PropertyKey.details)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let name = aDecoder.decodeObject(forKey:
+            PropertyKey.name) as? String else {
+                os_log("Unable to decode name", log: OSLog.default,
+                                                    type: .debug)
+                return nil
+        }
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo)
+            as? UIImage
+        let details = aDecoder.decodeObject(forKey: PropertyKey.details)
+            as? String
+        self.init(name: name, photo: photo, details: details!)
     }
 }
